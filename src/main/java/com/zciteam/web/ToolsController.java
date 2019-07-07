f121cb0faa2957d00df9903f74dd4ee2a781d33b
wndt <lenovo993721@outlook.com>
2019-07-07 23:34:38 +0800

    no message

--- src/main/java/com/zciteam/web/ToolsController.java
+++ src/main/java/com/zciteam/web/ToolsController.java
@@ -5,14 +5,11 @@ import com.zciteam.enums.CodeEnum;
 import com.zciteam.service.impl.ToolsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.Map;
 
