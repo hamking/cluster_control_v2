4766a36a04a6f2258081caf831cb5d33c594a4e8
wndt <lenovo993721@outlook.com>
2019-07-18 23:22:34 +0800

    分离

--- src/main/webapp/js/jiaoben.js
+++ src/main/webapp/js/jiaoben.js
@@ -1,3 +1,4 @@
+var jianben;
 $(function () {
     var id = getQueryString("id");
     var name = getQueryString("name");
@@ -192,6 +193,36 @@ $(function () {
         $("#"+use_id).show();
     });
 
+    (function () {
+        jianben ={
+            doExecute:function (suid) {
+                var data = {
+                    "scope": scope,
+                    "suid":suid,
+                    "uuid":id
+                };
+                $.ajax({
+                    url:'/script/run',
+                    type:'post',
+                    data:data,
+                    dataType:'json',
+                    success:function(datas){
+                        if(datas.code == 200){
+                            alert("正在执行！");
+                            $("#test").hide();
+                            $('#log_window').hide();
+                            $(".show-type").hide();
+                        }else{
+                            alert("执行失败!");
+                        }
+                    },error:function () {
+                        alert("服务器异常");
+                    }
+                })
+            }
+        }
+    })();
+
     this.getScriptList = function () {
         $.ajax({
             url:'/script/getScriptList',
@@ -228,266 +259,6 @@ $(function () {
            alert("数据异常！");
                }
     })
-    };
-
-    $(this).delegate('.script-zi','click',function(){
-        var suid = $(this).attr("data-id");
-        $.ajax({
-            url:'/script/getScript?suid='+suid,
-            type:'get',
-            dataType:'json',
-            success:function(datas){
-                if(datas.code == 200){
-                    $(".do-zhi").attr("data-id",datas.data.suid);
-                    $(".do-ting").attr("data-id",datas.data.suid);
-
-                    if(suid == "ComSmileGifmakerRaises"){
-                        $("#sou_"+datas.data.directMessagesType).click();
-                        if(datas.data.isGiveLike == 1){
-                            $("#dianzan").attr("checked",true);
-                        }
-                        if(datas.data.isComment == 1){
-                            $("#pinglun").attr("checked",true);
-                        }
-                        if(datas.data.isFocus == 1){
-                            $("#guanzhuping").attr("checked",true);
-                        }
-                        $("#shicnt").val(datas.data.focusNum);
-                        if(datas.data.isFocusAuthor == 1){
-                            $("#guanzuozhe").attr("checked",true);
-                        }
-                        $("#meichang").val(datas.data.watchTimeInterval);
-                        $("#jiange").val(datas.data.watchTime);
-                        $("#allkan").val(datas.data.watchNum);
-                        $("#nge").val(datas.data.numStart);
-                        $("#pinghuashu").val(datas.data.commentStr);
-                        $("#test").show();
-                        $("#log_window").show();
-                        $(".yang-hao").show();
-                    }else if(suid == "ComSmileGifmakerPrivateMsg"){
-                        $("#sizong").val(datas.data.watchNum);
-                        $("#xindien").val(datas.data.numStart);
-                        $("#xinmeitiao").val(datas.data.directMessagesNum);
-                        $("#si_"+datas.data.isDirectMessagesOnAuthor).click();
-                        $("#sixinhuashu").val(datas.data.directMessages);
-                        $('#simg').attr('src',datas.data.directMessagesImage);
-                        $(".si-xin").show();
-                        $("#test").show();
-                        $("#log_window").show();
-                    }else if(suid == "ComUgcAwemeRaises"){
-                        if(datas.data.isGiveLike == 1){
-                            $("#dou-zan").attr("checked",true);
-                        }
-                        if(datas.data.isComment == 1){
-                            $("#dou-lun").attr("checked",true);
-                        }
-                        if(datas.data.isFocus == 1){
-                            $("#dou-zhuping").attr("checked",true);
-                        }
-                        $("#shi-cnt").val(datas.data.focusNum);
-                        if(datas.data.isFocusAuthor == 1){
-                            $("#guan-zuo").attr("checked",true);
-                        }
-                        $("#mei-chang").val(datas.data.watchTimeInterval);
-                        $("#jian-ge").val(datas.data.watchTime);
-                        $("#all-kan").val(datas.data.watchNum);
-                        $("#n-ge").val(datas.data.numStart);
-                        $("#ping-hua").val(datas.data.commentStr);
-                        $("#test").show();
-                        $("#log_window").show();
-                        $(".dou-hao").show();
-                    }else if(suid == "ComUgcAwemePrivateMsg"){
-                        $("#guan_"+datas.data.directMessagesType).click();
-                        $("#xin-hua").val(datas.data.directMessages);
-                        $('#si-mg').attr('src',datas.data.directMessagesImage);
-                        $(".dou-xin").show();
-                        $("#test").show();
-                        $("#log_window").show();
-                    }
-                }else{
-                    alert("服务器异常");
-                }
-            }
-        })
-    });
-
-    $(this).delegate('.do-zhi','click',function(){
-        var suid = $(this).attr("data-id");
-        if(suid == "ComSmileGifmakerRaises"){
-            var directMessagesType = $('.tongcheng input[type="radio"]:checked').val();
-            if($("#dianzan").is(':checked') == true){
-                var isGiveLike = 1;
-            }else{
-                var isGiveLike = 0;
-            }
-            if($("#pinglun").is(':checked') == true){
-                var isComment = 1;
-            }else{
-                var isComment = 0;
-            }
-            if($("#guanzhuping").is(':checked') == true){
-                var isFocus = 1;
-            }else{
-                var isFocus = 0;
-            }
-            var focusNum = $("#shicnt").val();
-
-            if($("#guanzuozhe").is(':checked') == true){
-                var isFocusAuthor = 1;
-            }else{
-                var isFocusAuthor = 0;
-            }
-            var watchTimeInterval = $("#meichang").val();
-            var watchTime= $("#jiange").val();
-            var watchNum = $("#allkan").val();
-            var numStart = $("#nge").val();
-            var commentStr = $("#pinghuashu").val();
-            var directMessages = "";
-            var feedingTime = 0;
-            var isDirectMessages = 0;
-            var isDirectMessagesOnAuthor = 0;
-            var directMessagesNum = 0;
-            var directMessagesImage = "";
-        }else if(suid == "ComSmileGifmakerPrivateMsg"){
-            var watchNum = $("#sizong").val();
-            var numStart = $("#xindien").val();
-            var directMessagesNum = $("#xinmeitiao").val();
-            var isDirectMessagesOnAuthor = $('.sixinzi input[type="radio"]:checked').val();
-            var isDirectMessages = 0;
-            var directMessages = $("#sixinhuashu").val();
-            var directMessagesImage = $('#simg').attr('src');
-            var isFocus = 0;
-            var isFocusAuthor = 0;
-            var focusNum = 0;
-            var watchTime = 0;
-            var isComment = 0;
-            var isGiveLike = 0;
-            var commentStr = "";
-            var feedingTime = 0;
-            var watchTimeInterval = 0;
-            var directMessagesType = 0;
-        }else if(suid == "ComUgcAwemeRaises"){
-            var directMessagesType = 0;
-            if($("#dou-zan").is(':checked') == true){
-                var isGiveLike = 1;
-            }else{
-                var isGiveLike = 0;
-            }
-            if($("#dou-lun").is(':checked') == true){
-                var isComment = 1;
-            }else{
-                var isComment = 0;
-            }
-            if($("#dou-zhuping").is(':checked') == true){
-                var isFocus = 1;
-            }else{
-                var isFocus = 0;
-            }
-            var focusNum = $("#shi-cnt").val();
-
-            if($("#guan-zuo").is(':checked') == true){
-                var isFocusAuthor = 1;
-            }else{
-                var isFocusAuthor = 0;
-            }
-            var watchTimeInterval = $("#mei-chang").val();
-            var watchTime= $("#jian-ge").val();
-            var watchNum = $("#all-kan").val();
-            var numStart = $("#n-ge").val();
-            var commentStr = $("#ping-hua").val();
-            var directMessages = "";
-            var feedingTime = 0;
-            var isDirectMessages = 0;
-            var isDirectMessagesOnAuthor = 0;
-            var directMessagesNum = 0;
-            var directMessagesImage = "";
-        }else if(suid == "ComUgcAwemePrivateMsg"){
-            var watchNum = 0;
-            var numStart = 0
-            var directMessagesNum = 0;
-            var isDirectMessagesOnAuthor = $('.guan-zi input[type="radio"]:checked').val();
-            var isDirectMessages = 0;
-            var directMessages = $("#xin-hua").val();
-            var directMessagesImage = $('#si-mg').attr('src');
-            var isFocus = 0;
-            var isFocusAuthor = 0;
-            var focusNum = 0;
-            var watchTime = 0;
-            var isComment = 0;
-            var isGiveLike = 0;
-            var commentStr = "";
-            var feedingTime = 0;
-            var watchTimeInterval = 0;
-            var directMessagesType = 0;
-        };
-        var data = {
-            "suid":suid,
-            "isFocus":isFocus,
-            "isFocusAuthor":isFocusAuthor,
-            "watchNum":watchNum,
-            "numStart":numStart,
-            "focusNum":focusNum,
-            "watchTime":watchTime,
-            "isComment":isComment,
-            "isGiveLike":isGiveLike,
-            "commentStr":commentStr,
-            "directMessages":directMessages,
-            "feedingTime":feedingTime,
-            "watchTimeMan":0,
-            "watchTimeMin":0,
-            "isDirectMessages":isDirectMessages,
-            "watchTimeInterval":watchTimeInterval,
-            "directMessagesNum":directMessagesNum,
-            "directMessagesImage":directMessagesImage,
-            "directMessagesType":directMessagesType,
-            "isDirectMessagesOnAuthor":isDirectMessagesOnAuthor,
-            "isOnlyDirectMessages":0,
-            "individuationString":"",
-            "individuationInt":0,
-            "individuationVar1":0,
-            "individuationVar2":0
-        };
-        $.ajax({
-            url:'/script/saveScriptSetting',
-            type:'post',
-            data:data,
-            dataType:'json',
-            success:function(datas){
-                if(datas.code == 200){
-                    doExecute(suid);
-                }else{
-                    alert("执行失败!");
-                }
-            },error:function () {
-                alert("服务器异常");
-            }
-        })
-    });
-
-    function doExecute(suid) {
-        var data = {
-            'scope': scope,
-            "suid":suid,
-            "uuid":id
-        };
-        $.ajax({
-            url:'/script/run',
-            type:'post',
-            data:data,
-            dataType:'json',
-            success:function(datas){
-                if(datas.code == 200){
-                   alert("正在执行！");
-                    $("#test").hide();
-                    $('#log_window').hide();
-                    $(".show-type").hide();
-                }else{
-                    alert("执行失败!");
-                }
-            },error:function () {
-                alert("服务器异常");
-            }
-        })
     };
 
     $(this).delegate('#test','click',function(){
