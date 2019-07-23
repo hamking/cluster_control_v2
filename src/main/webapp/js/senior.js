5476be1576da9e8c344e11588ae0cee973c47bd4
wndt <lenovo993721@outlook.com>
2019-07-23 17:22:19 +0800

    基本界面完成

--- src/main/webapp/js/senior.js
+++ src/main/webapp/js/senior.js
@@ -1,18 +1,9 @@
 $(function () {
-    var blocklyDiv = $("#blocklyDiv")[0];
-    var workspace = Blockly.inject(blocklyDiv,
+        var workspace = Blockly.inject('blockly_div',
         {toolbox: $("#toolbox")[0],
         zoom:
            {controls: true,
             wheel: true},
         trashcan: true,
         scrollbars: true});
-    var onresize = function(e) {
-        blocklyDiv.style.width = (window.innerWidth - 500) + 'px';
-        blocklyDiv.style.height = (window.innerHeight - 82 - 28) + 'px';
-        Blockly.svgResize(workspace);
-    };
-    window.addEventListener('resize', onresize, false);
-    onresize();
-    Blockly.svgResize(workspace);
-});
+})
